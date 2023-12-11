c4970e3072a0f5820146b404dae23d912a141440
whayido <dp10nice@gmail.com>
2023-12-11 11:03:24 +0700

    f

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/kAFe3Ra-W/">
     <App />
   </BrowserRouter>
 );
